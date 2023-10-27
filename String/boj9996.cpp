b074c159edd66a35f79efac759088d0a99a4cc0a
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-10-28 02:08:46 +0900

    Create boj9996.cpp

---
+++ String/boj9996.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int cnt;
+string pattern;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> cnt;
+    cin >> pattern;
+
+    while (cnt--) {
+        string str;
+        cin >> str;
+
+        
+
+
+    }
+
+    return 0;
+}
+
+// 패턴이 "AB*BD"이고 검사할 문자열이 "ABD"라면 어떨까요?
+// 1. AB 인지 확인한다
+// 2. 마지막 BD가 있는지 확인한다.
